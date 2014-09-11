bda20132f227bff45805f1e6638978b86adca15b
Rohan <sarith21@gmail.com>
2014-09-11 16:09:10 -0700

    Fix error behaviour on loading modules and update version

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-/*
-features to highlight:
-- multiple require statements -> implement unique counter
-- requirements that are never used -> more fancy regex... do this last
-- dependency cycles -> check global Dependencies
-*/
-
-
 var args  = process.argv.slice(2);
 var dTree = require('./dTree.js');
 var fs    = require('fs');
 var fetch = require('./fetchDependencies.js');
+var colors = require('colors');
+
+try{
+  fs.readFileSync(filePath, 'utf8');
+}
+catch(e){
+  console.log("Undefined or invalid filePath.".red);
+  console.log("E.g., use: dtree pathToYourFile.js \n".red);
+  process.exit();
+}
 
 function readRecurse(filePath){
   var file = fs.readFileSync(filePath, 'utf8');
